5b350dfc8f9966e271830d1d1d41b23f7332348f
HangNT169 <104345338+HangNT169@users.noreply.github.com>
2023-09-13 09:09:45 +0700

    Add files via upload

---
+++ Buoi2_KieuDuLieu.c
@@ -0,0 +1,40 @@
+//B1: Khai bao thu vien 
+#include<stdio.h> 
+// B2: Viet main 
+// Tat ca cac code => Main tren return 
+int main(){
+    // Comment => Boi den doan muon comment 
+    // Roi an Ctrl / . Bo comment an Ctrl /
+    // Code 
+    // In ra man hinh => printf 
+    // Ket thuc dong lenh la dau cham phay
+    // Muon xuong dong => \n 
+    // Muon lui vao 1 tab => \t
+    // https://viettuts.vn/lap-trinh-c/ky-tu-dac-biet-trong-c 
+    // printf("Ahihi\n");
+    // printf("\tMet qua");
+    // Bai tap: In thong tin gom ten, tuoi,
+    //dia chi, que quan cua ban than ra man hinh
+    printf("nguyenxuantrieu\n");
+    printf("sinhnam2005\n");
+    printf("yenphu,vanphu,thuongtin,hanoi");
+    // Kieu du lieu 
+    // int/long => so nguyen : 1 ,2,3...
+    // float/double => so thuc : 1.1,1.2....
+    // char => chuoi 
+    // bool => true/false 
+    // Cach khai bao bien 
+    // Kieu du lieu tenBien;
+    // VD: int a;
+    // Khoi tao gia tri cho bien 
+    // Vd: Khai bao 1 so thuc co gia tri khoi tao la 5.5
+    // float b = 5.5;
+    // float b=4.6;
+    // float b = 
+    // Convention:
+    // Co 2 quy tac dat ten bien :
+    // ran : _ VD: number_first
+    // lac da: numberFirst 
+    // Dat ten bien co nghia 
+    return 0;
+}
